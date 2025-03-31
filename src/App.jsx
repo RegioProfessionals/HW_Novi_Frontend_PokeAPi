9a80bd974fd3519efbca919ec8e7fdf129b55788
RegioProfessionals <richardvanbeem@live.nl>
2025-03-31 16:13:40 +0200

    aangepast tot aan het aanpassen van de PokemonCard voor 20 items, daar gaat het mis

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import './App.css'
+import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from "react";
+import PokemonCard from "./components/pokemonCard/PokemonCard.jsx";
 
 function App() {
 
-  return (
-    <>
-      <h1>Gotta catch em all!</h1>
-    </>
-  )
+    const [pokemonTwenty, setPokemonTwenty] = useState([]);
+
+    async function fetchPokemon() {
+         try {
+         const result = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+         setPokemonTwenty(result.data.results);
+         } catch (e) {
+           console.error(e);
+         } finally {
+         }
+    }
+    useEffect(() => {
+     fetchPokemon();
+     }, []);
+console.log(pokemonTwenty);
+    if (!pokemonTwenty[0]) {
+        return (
+            <div>
+                <p>Loading ...</p>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <h1>Gotta catch em all!</h1>
+            {pokemonTwenty && <PokemonCard pokemonData={pokemonTwenty} />}
+            {pokemonTwenty?.map((name, index) => {
+                return (
+                <div key={name.url}>
+                <p>{name.name}</p>
+                <p>{name.url}</p>
+                </div>
+            );
+            })}
+        </>
+    )
 }
 
 export default App
---
+++ src/components/pokemonCard/PokemonCard.jsx
@@ -0,0 +1,48 @@
+import './PokemonCard.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+function PokemonCard({pokemonData}) {
+    const [pokemon, setPokemon] = useState({});
+    console.log(`Dit is het resultaat van de selectie van 20: ${pokemonData}`);
+
+    async function fetchPokemonData() {
+    try {
+        const result = await axios.get('https://pokeapi.co/api/v2/pokemon/JigglyPuff/');
+        // console.log(result.data);
+        setPokemon(result.data);
+    } catch (e) {
+        console.error(e);
+    } finally {
+
+    }
+}
+useEffect(() => {
+    fetchPokemonData();
+}, []);
+
+    if (!pokemon.id) {
+        return (
+            <div>
+                <p>Loading ...</p>
+            </div>
+        )
+    }
+
+return (
+    <div>
+
+        <h2>{pokemon.id}. {pokemon.name}</h2>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}  alt={pokemon.name}/>
+        <p>Moves: {pokemon.moves?.length || 0}</p>
+        <p>Weight: {pokemon.weight}</p>
+        <p>Abilities:</p>
+        {pokemon.abilities?.map((ability, index) => (
+            <p key={index}>{ability.ability.name}</p>))}
+    </div>
+)
+}
+
+export default PokemonCard;
+
+
